8eff98166a732ad667a9c3751c89b61c1a692aaf
githubpython2 <102801584+githubpython2@users.noreply.github.com>
2022-04-01 17:10:50 +0500

    Add files via upload

---
+++ Homewor_10.py
@@ -0,0 +1,11 @@
+dastur = input("Dasturlash tilini kiriting:")
+
+dastur_1 = ["html" , "Css" , "Javaskrip" , "Python" , "C#" ,"C++" ,"Nodjes" ,"Djengo" ,"Sisco" ,"Bacend" , "Sirojbek"]
+
+print(dastur_1.index(dastur))
+
+
+
+
+
+
